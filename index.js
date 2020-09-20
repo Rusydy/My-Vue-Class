57d2a8ceaeaf7a45280eea3dc742e5b766fade3a
Rusydy <rusydymuhiddin@gmail.com>
2020-09-20 20:41:18 +0700

    Binding Data & Simple To-Do Example

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ new Vue({
     el: '#app',
 
     data: {
-        title: 'My title',
-        content: 'Here is my text.',
-        menu: 'My menu data here'
+        item: '',
+        items: [
+            'item 1',
+            'item 2',
+        ],
     }
 })
