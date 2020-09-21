d028e6d6e45556d1b51e69a40c7faeb0c2c1158d
Rusydy <rusydymuhiddin@gmail.com>
2020-09-21 12:40:01 +0700

    Computed Properties

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ new Vue({
     el: '#app',
 
     data: {
-        item: '',
-        items: [
-            'item 1',
-            'item 2',
-        ],
+        price: 100,
+    },
+
+    computed: {
+        tax: function () {
+            return this.price * 0.1
+        },
+
+        taxFormatted: function() {
+            return `$${this.tax}`
+        },
+
+        total: function () {
+            return parseInt(this.price) + this.tax
+        },
+
+        totalFormatted: function() {
+            return `$${this.total}`
+        }
     }
 })
