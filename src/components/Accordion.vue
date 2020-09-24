3324150478b4f582ba7cf91b6bb31712ffea0d4c
Rusydy <rusydymuhiddin@gmail.com>
2020-09-24 15:42:04 +0700

    NPM, Webpack & Single File Component

---
+++ src/components/Accordion.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>{{ item.title }}</p>
+        <button @click=" toggle = ! toggle" >Details</button>
+        <p v-if="toggle">{{ item.description }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Accordion",
+
+        props: ['item'],
+
+        data: function (){
+            return {
+                toggle: false
+            }
+        }
+    }
+</script>
