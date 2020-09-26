2ff2dd1bf127e504146b26b7e740f16875589b7f
Rusydy <rusydymuhiddin@gmail.com>
2020-09-26 23:23:14 +0700

    Making HTTP Requests with Axios, API Example

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 // import Accordion from './components/Accordion'
-import List from './components/List'
+// import List from './components/List'
+
+window.axios = require('axios')
 
 new Vue({
     el: '#app',
 
     components: {
-        List
+        
+    },
+
+    mounted: function () {
+
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => this.posts = response.data)
+            .catch(error => this.posts = [{title: 'No posts found.'}])
+            .finally(() => console.log('Data loading complete.'))
+
     },
 
     data: {
-        //
+        posts: null,
     }
 })
